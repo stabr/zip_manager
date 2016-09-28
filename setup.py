d4600dc4258edac3310dbf68b6ea299e76707886
stabr <syrbor@gmail.com>
2016-09-28 22:46:06 +0300

    add more acsident information

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 import os
-import sys
 import json
 
 def config(key):
-    value = ''
-    data_file = os.path.dirname(__file__)+'/data.json'
-    if os.path.exists(data_file):
-        try:
-                    value = json.load(open(data_file))[key]
-        except Exception, e:
+    value = "\nCan`t get any data file!\n"
+    data = os.path.normpath(os.path.join(os.path.dirname(__file__), 'data.json'))
+    if os.path.exists(data):
+                try:
+                    value = json.load(open(data))[key]
+                except Exception, e:
                     value = e
-    # sys.platform
     return value
